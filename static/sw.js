20e7f4921fbe05c2e095614abbd3705585259d5c
station-model-drawer <50741124+station-model-drawer@users.noreply.github.com>
2019-06-12 22:00:34 +0000

    Initial commit

---
+++ static/sw.js
@@ -0,0 +1,48 @@
+importScript('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js');
+
+workbox.routing.registerRoute(
+	/\.(css|js|html)/,
+	new workbox.strategies.StaleWhileRevalidate({
+		cacheName: "dynamic-resources"
+	})
+);
+
+workbox.routing.registerRoute(
+	/\.svg/,
+	new workbox.strategies.CacheFirst({
+		cacheName: "svg",
+		plugins: [
+			new workbox.expiration.Plugin({
+				maxEntries: 20,
+				maxAgeSeconds: 7 * 24 * 60 * 60
+			})
+		]
+	})
+);
+
+// Cache certain third-party resources from cdn
+workbox.routing.registerRoute(
+	({url}) => {
+		const {origin} = new URL(url);
+		if (origin === "https://use.fontawesome.com" || origin === "https://cdn.jsdelivr.net") {
+			return true;
+		}
+	},
+	new workbox.strategies.StaleWhileRevalidate({
+		cacheName: "cdn-resources"
+	})
+);
+
+workbox.routing.registerRoute(
+	/.woff2?/,
+	new workbox.strategies.CacheFirst({
+		cacheName: "font",
+		plugins: [
+			new workbox.expiration.Plugin({
+				maxEntries: 20,
+				maxAgeSeconds: 30 * 24 * 60 * 60
+			})
+		]
+	})
+);
+
